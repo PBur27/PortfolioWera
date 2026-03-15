76c151537dd619c57de8d64346d7fcd1415c6071
PBur27 <p.burda2003@gmail.com>
2026-03-15 20:52:41 +0100

    apikey

---
+++ functions/api/mailing.js
@@ -0,0 +1,23 @@
+import { Resend } from "resend";
+
+export async function onRequestPost(context) {
+  const resend = new Resend("context.env.RESEND_API_KEY");
+
+  try {
+    const { sender, content } = await context.request.json();
+
+    const data = await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: "p.burda2003@gmail.com",
+      reply_to: sender,
+      subject: `Portfolio Mailing - Contact request from ${sender}`,
+      html: `<p>${content}</p>`,
+    });
+
+    return Response.json({ success: true, data });
+  } catch (error) {
+    return Response.json({ error: error.message }, { status: 500 });
+  }
+}
+
+
